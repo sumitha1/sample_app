eeff0e8ef9499db23fcd534eb08645715f98f8bb
sumitha1 <sumitha1@umbc.edu>
2012-03-08 21:15:11 -0500

    new logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  def logo
+    # Fill in.
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
