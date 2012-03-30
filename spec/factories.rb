0bc390e70af095f88fc8efe66e578a71bd677082
sumitha1 <sumitha1@umbc.edu>
2012-03-30 10:34:07 -0300

    Update spec/factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                  "Sumi Ambati"
+  user.name                  "Sumitha Ambati"
   user.email                 "sumitha1@umbc.edu"
   user.password              "helloa12"
   user.password_confirmation "helloa12"
