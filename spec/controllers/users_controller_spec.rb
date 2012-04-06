5831892ab5c033ae57247ff87d348541059a2830
sumitha1 <sumitha1@umbc.edu>
2012-04-05 20:31:17 -0400

    chap 8 exer

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,26 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[email='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[password='user[password]'][type='text']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[password_confirmation='user[password_confirmation]'][type='text']")
+    end
   end
 
   describe "GET 'show'" do
